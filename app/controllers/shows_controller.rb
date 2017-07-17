4c1b385b5b8d29b45b6f43ad04e036658a0ba1f3
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-07-17 19:41:00 +0100

    add shows_controller with index and create routes

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -5,4 +5,15 @@ class ShowsController < ApplicationController
     render :json => shows
   end
 
+  def create
+    new_show = Show.create( show_params )
+    render :json => new_show
+  end
+
+end
+
+private
+
+def show_params
+  params.require(:show).permit([:title, :series, :description, :image])
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Show.delete_all
+
+Show0 = {
+  "title": "The Great British Bake Off",
+  "series": 7,
+  "description": "Master bakers Mary & Paul and the incomparable presenting duo of Mel & Sue are back for another interesting series about baking cakes.",
+  "image": "placeholder.jpg",
+  "programmeID": "b013pqnm" 
+}
+
+Show1 = {
+  "title": "Game Of Thrones",
+  "series": 7,
+  "description": "Game of Thrones is an American fantasy drama television series created by David Benioff and D. B. Weiss. It is an adaptation of A Song of Ice and Fire, George R. R. Martin's series of fantasy novels, the first of which is A Game of Thrones.",
+  "image": "placeholder.jpg",
+  "programmeID": "gam30fth0n35" 
+}
+
+Show2 = {
+  "title": "The Great British Bake Off",
+  "series": 9,
+  "description": "The series, centred on the sport of robot combat, involved teams of amateur and professional roboteers operating their own constructed 
+  robots to fight against each other in both friendly and tournament matches, whilst also avoiding arena hazards and more powerful \"House Robots\", which were not bound by the same weight or weapon limits as the contestants. Earlier series included assault and trial courses for competing robots before they were ceased from the first \"Extreme\" series onwards",
+  "image": "placeholder.jpg",
+  "programmeID": "Ki11em@11" 
+}
+
+Show.create( Show0)
+Show.create( Show1)
+Show.create( Show2)
